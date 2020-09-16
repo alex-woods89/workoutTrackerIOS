af955ed0439fceb35af60597a9aef948f812b2e9
Alex Woods <alex.woods89@hotmail.co.uk>
2020-09-16 08:39:28 +0100

    add print statement

--- workoutTracker/ViewControllers/WorkoutOneViewController.swift
+++ workoutTracker/ViewControllers/WorkoutOneViewController.swift
@@ -12,7 +12,7 @@ class WorkoutOneViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("statement")
         // Do any additional setup after loading the view.
     }
     
