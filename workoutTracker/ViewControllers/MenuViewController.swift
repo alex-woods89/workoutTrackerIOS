014522326d3e333687258027173740c7f936fe46
alex-woods89 <52571445+alex-woods89@users.noreply.github.com>
2020-09-16 08:27:15 +0100

    first commit

---
+++ workoutTracker/ViewControllers/MenuViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  workoutTracker
+//
+//  Created by Alex Woods on 15/09/2020.
+//  Copyright © 2020 Alex Woods. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "showWorkoutOneSegue" {
+//            if let destinationVC = segue.destination as? workoutOneViewController {
+//                print("About to segue to workout one!!")
+//            }
+//        }
+//    }
+}
+
---
+++ workoutTracker/ViewControllers/WorkoutThreeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WorkoutThreeViewController.swift
+//  workoutTracker
+//
+//  Created by Alex Woods on 15/09/2020.
+//  Copyright © 2020 Alex Woods. All rights reserved.
+//
+
+import UIKit
+
+class WorkoutThreeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
